9000dbecdb221815b9f793839684de3c27ed8856
amariet <785037@seq.org>
2016-08-25 16:06:44 -0700

    Update FirstClassRunner.java

--- APCSLabs/FirstClassRunner.java
+++ APCSLabs/FirstClassRunner.java
@@ -8,7 +8,7 @@ public class FirstClassRunner
     public static void main(String[] args)
     {
         FirstClass fc = new FirstClass();
-        System.out.println("Numbers: " + a + ", " + b);
+        System.out.println("Numbers: " + 2 + ", " + 5);
         System.out.println("The Sum is: " + fc.addEmUp());
         System.out.println("The Difference is: " + fc.difference());
     }
