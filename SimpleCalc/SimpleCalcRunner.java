eeff5cf7c890731ccdd6446a9cbf3c57b317f202
amariet <785037@seq.org>
2016-08-25 11:58:58 -0700

    Update SimpleCalcRunner.java

--- SimpleCalc/SimpleCalcRunner.java
+++ SimpleCalc/SimpleCalcRunner.java
@@ -13,7 +13,7 @@ public class SimpleCalcRunner
         System.out.println("Numbers: " + n1 + ", " + n2);
         System.out.println("The Sum is: " + sc.add());
         System.out.println("The Diff is: " + sc.subtract());
-        System.out.println("TheProduct is: " + sc.mult());
+        System.out.println("The Product is: " + sc.mult());
         System.out.println("The Qoutient is: " + sc.div());
         System.out.println("n1 greater than n2: " + sc.isGreater());
      
