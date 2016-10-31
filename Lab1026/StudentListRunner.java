edfb7678c1c3c2ba1e02e8d30b4d682c9cd3ec38
amariet <785037@seq.org>
2016-10-31 08:48:34 -0700

    Update StudentListRunner.java

--- Lab1026/StudentListRunner.java
+++ Lab1026/StudentListRunner.java
@@ -3,20 +3,50 @@
  * October 26, 2016
  */
 import java.util.Scanner;
+import java.util.Arrays;
 public class StudentListRunner
 {
     public static void main(String[] args)
     {
         Scanner scr = new Scanner(System.in);
-        String[] menu = new String[1];
-        StudList studList = new StudList(3);
-        menu[0] = "Add a student";
+        StudentList sl = new StudentList(3);
         String ans = "";
-        System.out.print("Choose an option from the menu by typing its corresponding number.");
-        while(ans.equals("quit"))
+        String myString;
+        int myInt;
+        double myDouble; 
+        Student student;
+        int quit = 0;
+        while(quit == 0)
         {
-            System.out.print();
-            ans = scr.next
+            System.out.print("Choose an option from the menu by typing its corresponding number.");
+            System.out.println("Add a student (1)");
+            System.out.println("Quit (0)");
+            ans = scr.next();
+            if(ans.equals("1"))
+            {
+                System.out.println("What is the student's name?");
+                myString = scr.next();
+                System.out.println("What is the student's ID?");
+                myInt = scr.nextInt();
+                System.out.print("What is the student's GPA?");
+                myDouble = scr.nextDouble();
+                //Student stu = new Student(myString, myInt, myDouble);
+                //sl.addStudent(stu);
+            }
+            else if(ans.equals("2"))
+            {
+                Student stuList = sl.getStudentList();
+                for(int i = 0; i < stuList.length - 1; i++)
+                {
+                    System.out.println("Name: " + stuList[1].getName());
+                    System.out.println("ID: " + stuList[2].getID());
+                    System.out.println("GPA: " + stuList[3].getGPA());
+                }
+            }
+            else if(ans.equals("0"))
+            {
+                quit = -1;
+            }
         }
     }
 }
