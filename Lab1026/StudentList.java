30887876e146d4fa00aa010751c8dab8a1f0f53d
amariet <785037@seq.org>
2016-10-31 08:48:55 -0700

    Update StudentList.java

--- Lab1026/StudentList.java
+++ Lab1026/StudentList.java
@@ -8,11 +8,24 @@ public class StudentList
 
     public StudentList(int numberOfStudents)
     {
-        students = new Student(numberOfStudents);
+        students = new Student[numberOfStudents];
     }
     
-    public void addStudent(Student s, int index,)
+    public void addStudent(Student stu)
     {
-        students[index] = s;
+        int ans = -1;
+        for(int i = students.length - 1; i >= 0; i--)
+        {
+            if(students[i] == null)
+            {
+                ans = i;
+            }
+        }
+        students[ans] = stu;
+    }
+    
+    public Student[] getStudentList()
+    {
+        return students;
     }
 }
